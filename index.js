66fdc10113482b33a44f1c7f0dd6d2668f56a33e
crutland <crutland@amherst.com>
2018-11-12 14:24:02 -0600

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const thingName = 'AIL_IoT_RPi_01';
 const thingHost = 'a3knx5ouu01ymf-ats.iot.us-east-1.amazonaws.com';
 
 
-const led12 = new LedManager(12);
+//const led12 = new LedManager(12);
 
 function handleDelta(payload, shadow) {
     console.log("delta", payload, shadow);
