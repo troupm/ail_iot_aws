cc59e0dffac8e753fde8a9620b2e681ae2d3c90a
crutland <crutland@amherst.com>
2018-11-12 12:36:16 -0600

    added certs

--- deviceDefaults.js
+++ deviceDefaults.js
@@ -1,7 +1,7 @@
 module.exports = {
-  keyPath: "AIL_IoT_RPi_01.private.key",
-  certPath: "AIL_IoT_RPi_01.cert.pem",
-  caPath: "root-CA.crt",
+  keyPath: "certs/AIL_IoT_RPi_01.private.key",
+  certPath: "certs/AIL_IoT_RPi_01.cert.pem",
+  caPath: "certs/root-CA.crt",
   port: 8883,
   region: "us-east-1"
 }
