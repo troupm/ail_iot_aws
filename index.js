5d01ce319a44fe3a74918afc3e5a6dbd41e8d933
crutland <crutland@amherst.com>
2018-11-12 13:29:53 -0600

    added led subscription

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const LedManager = require("./ledManager");
 const thingName = 'AIL_IoT_RPi_01';
 const thingHost = 'a3knx5ouu01ymf-ats.iot.us-east-1.amazonaws.com';
 
-new DeviceManager({ clientId: thingName, host: thingHost });
+new DeviceManager({ clientId: thingName, host: thingHost }, ["LED"]);
