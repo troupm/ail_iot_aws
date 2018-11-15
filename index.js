c903514329283c5bbe7ca5db49d5f1ad5d03b774
tmiller <tmiller@amherst.com>
2018-11-14 18:39:53 -0600

    STABLE: With Sensor: temp & temSetpoint

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const LedManager = require("./ledManager");
 const thingName = 'AIL_IoT_RPi_01';
 const thingHost = 'a3knx5ouu01ymf-ats.iot.us-east-1.amazonaws.com';
 
-
 const led12 = new LedManager(12);
 
 function handleDelta(thingName, payload, shadow) {
